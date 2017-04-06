be3581373ce7c52d5f3379d0b0d537d0d5f723e0
Igor Poubel <ipoubellopes@gmail.com>
2017-04-05 22:51:55 -0300

    Ajustes Banco de dados

---
+++ alteracoes_banco.sql
@@ -0,0 +1,3 @@
+ALTER TABLE portal_noticias.noticias ADD COLUMN resumo varchar(100);
+ALTER TABLE portal_noticias.noticias ADD COLUMN autor varchar(30);
+ALTER TABLE portal_noticias.noticias ADD COLUMN data_noticia date;
